93be9e95c18f5e56e7eac423a76f520a2782eaee
hainv26 <hainv26@onemount.com>
2021-05-16 23:30:15 +0700

    update code

---
+++ src/test/java/com/housing/features/search/SearchGithub.java
@@ -0,0 +1,46 @@
+package com.housing.features.search;
+
+import com.housing.tasks.OpenGitHub;
+import com.housing.tasks.Search;
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
+import net.serenitybdd.screenplay.questions.page.TheWebPage;
+import net.thucydides.core.annotations.Managed;
+import net.thucydides.core.annotations.Steps;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+
+import static net.serenitybdd.screenplay.EventualConsequence.eventually;
+import static net.serenitybdd.screenplay.GivenWhenThen.*;
+import static org.hamcrest.Matchers.containsString;
+
+@RunWith(SerenityRunner.class)
+public class SearchGithub {
+
+    Actor hainv = Actor.named("Hainv");
+
+    @Managed(uniqueSession = true)
+    public WebDriver browser;
+
+    @Steps
+    OpenGitHub openGitHub;
+
+    @Before
+    public void annaCanBrowseTheWeb() {
+        hainv.can(BrowseTheWeb.with(browser));
+    }
+
+    @Test
+    public void search_results_should_show_the_search_term_in_the_title() {
+
+        givenThat(hainv).wasAbleTo(openGitHub);
+
+//        when(hainv).attemptsTo(Search.forTheTerm("BDD In Action"));
+//
+//        then(hainv).should(eventually(seeThat(TheWebPage.title(), containsString("BDD In Action"))));
+
+    }
+}
---
+++ src/test/java/com/housing/ui/GithubPage.java
@@ -0,0 +1,8 @@
+package com.housing.ui;
+
+import net.serenitybdd.core.pages.PageObject;
+import net.thucydides.core.annotations.DefaultUrl;
+
+@DefaultUrl("https://github.com/")
+public class GithubPage extends PageObject {
+}
