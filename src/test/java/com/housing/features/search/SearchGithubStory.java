9b5842f3bfda78d185e7ffa85f32d36fe56d81ff
hainv26 <hainv26@onemount.com>
2021-05-20 23:13:20 +0700

    delete all code, retry code...

--- src/test/java/com/housing/features/search/SearchGithubStory.java
+++ src/test/java/com/housing/features/search/SearchGithubStory.java
@@ -1,45 +1,29 @@
 package com.housing.features.search;
 
-import com.housing.tasks.OpenGitHub;
-import com.housing.tasks.Search;
-import com.housing.tasks.SearchGithub;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
-import net.serenitybdd.screenplay.questions.page.TheWebPage;
 import net.thucydides.core.annotations.Managed;
-import net.thucydides.core.annotations.Steps;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
-
-import static net.serenitybdd.screenplay.EventualConsequence.eventually;
-import static net.serenitybdd.screenplay.GivenWhenThen.*;
-import static org.hamcrest.Matchers.containsString;
 
 @RunWith(SerenityRunner.class)
 public class SearchGithubStory {
 
     Actor hainv = Actor.named("Hainv");
 
-    @Managed(uniqueSession = true)
-    public WebDriver browser;
-
-    @Steps
-    OpenGitHub openGitHub;
+    @Managed()
+    public WebDriver hisBrowser;
 
     @Before
-    public void annaCanBrowseTheWeb() {
-        hainv.can(BrowseTheWeb.with(browser));
+    public void initHisBrowser() {
+        hainv.can(BrowseTheWeb.with(hisBrowser));
     }
 
     @Test
-    public void search_results_should_show_the_search_term_in_the_title() {
-
-        givenThat(hainv).wasAbleTo(openGitHub);
-        when(hainv).attemptsTo(SearchGithub.forKeyword("haibgit"));
-        then(hainv).should(eventually(seeThat(TheWebPage.title(), containsString("haibgit"))));
+    public void test_first_demo() {
 
     }
 }
--- src/test/java/com/housing/ui/GithubPage.java
+++
@@ -1,8 +0,0 @@
-package com.housing.ui;
-
-import net.serenitybdd.core.pages.PageObject;
-import net.thucydides.core.annotations.DefaultUrl;
-
-@DefaultUrl("https://github.com/")
-public class GithubPage extends PageObject {
-}
